305385621d207186fb63723093f04f15a5ac309f
Aijaz Khan <aijaz-khan@hotmail.com>
2017-03-27 14:15:00 +0500

    Chnages in db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170316134433) do
+ActiveRecord::Schema.define(version: 20170327085622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,20 @@ ActiveRecord::Schema.define(version: 20170316134433) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "vehicles", force: :cascade do |t|
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.string   "name"
+    t.string   "vehicle_type"
+    t.integer  "model_year"
+    t.integer  "seats"
+    t.string   "color"
+    t.string   "origin_country"
+    t.float    "price"
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_vehicles_on_user_id", using: :btree
   end
 
 end
