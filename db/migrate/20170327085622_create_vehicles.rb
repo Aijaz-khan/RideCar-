f90b5f754c179f734b575a0606f7e9dbe6dd8238
Aijaz Khan <aijaz-khan@hotmail.com>
2017-03-27 14:14:42 +0500

    Car attributes added in car model

--- db/migrate/20170327085622_create_vehicles.rb
+++ db/migrate/20170327085622_create_vehicles.rb
@@ -3,6 +3,14 @@ class CreateVehicles < ActiveRecord::Migration[5.0]
     create_table :vehicles do |t|
 
       t.timestamps
+      t.string :name
+      t.string :vehicle_type
+      t.integer :model_year
+      t.integer :seats
+      t.string :color
+      t.string :origin_country
+      t.float :price
+      t.references(:user, index: true)
     end
   end
 end
